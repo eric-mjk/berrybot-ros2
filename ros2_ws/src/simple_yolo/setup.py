b400bc01a3226bc4671cde6996df1b8b5766c570
eric-mjk <eric.mjkim35@gmail.com>
2025-07-29 18:30:34 +0900

    Checkpoint1

--- ros2_ws/src/simple_yolo/setup.py
+++ ros2_ws/src/simple_yolo/setup.py
@@ -1,26 +1,30 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'simple_yolo'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(include=[package_name, f'{package_name}.*']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         [f'resource/{package_name}']),
         ('share/' + package_name, ['package.xml']),
+        (f'share/{package_name}/models', glob(f'{package_name}/models/*')),
+        (f'share/{package_name}/ultralytics/ultralytics/cfg', glob(f'{package_name}/ultralytics/ultralytics/cfg/*.yaml')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mj',
     maintainer_email='eric.mjkim35@gmail.com',
-    description='simple_yolo_pkg',
+    description='YOLO integrated package',
     license='Apache-2.0',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "camera = simple_yolo.camera:main"
+            'perception_node = simple_yolo.perception_node:main',
         ],
     },
 )
