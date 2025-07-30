4cd480c922f166eaf2e3fd178163d7e3af8967a8
eric-mjk <eric.mjkim35@gmail.com>
2025-07-30 14:13:43 +0900

    edit

--- ros2_ws/src/base_python/setup.py
+++ ros2_ws/src/base_python/setup.py
@@ -1,26 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'simple_yolo'
+package_name = 'base_python'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mj',
     maintainer_email='eric.mjkim35@gmail.com',
-    description='YOLO integrated package',
+    description='TODO: Package description',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'camera = simple_yolo.camera:main',
-            'perception_node = simple_yolo.perception_node:main',
         ],
     },
 )
